65b200834852eec5362270b1c68635e302f923ab
Himanshu Bhatt <hbhatt034@gmail.com>
2025-04-15 12:20:02 +0530

    Modified backend.tf file

--- Infrastructure Setup/backend.tf
+++ Infrastructure Setup/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mern-application-bucket"
+    bucket         = "mern-application-terraform-bucket"
     region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "terraform-state-lock-mern-application"
