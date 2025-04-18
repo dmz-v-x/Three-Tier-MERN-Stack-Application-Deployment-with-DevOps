76ad3d7736c95bef7b4e2b0f438310d5c84681f6
Himanshu Bhatt <hbhatt034@gmail.com>
2025-04-18 18:54:18 +0530

    Modified App.tsx

--- NoteFlow/frontend/src/App.tsx
+++ NoteFlow/frontend/src/App.tsx
@@ -29,7 +29,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/notesflow">
+      <BrowserRouter>
         <AuthProvider>
           <NotesProvider>
             <Routes>
